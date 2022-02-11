34293f486a26063b7eb499e366a1e75ca352ca8a
Mateo Rodriguez <mateoytomas2009@hotmail.com>
2022-02-10 23:44:55 -0300

    Implementacion funcional del movimiento de la pelota, se agrega la colision de la pelota con las barras y con los bordes del tablero, se agregan indicaciones de juego

--- main.js
+++ main.js
@@ -27,6 +27,9 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
         board.ball = this;
         this.kind = "circle";
     }
@@ -35,6 +38,33 @@
         move: function(){ // Movimiento de la pelota
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+        },
+        get width(){
+            return this.radius * 2;
+        },
+        get height(){
+            return this.radius * 2;
+        },
+        collision: function(bar){
+        // Reaccion de colision con una barra que se recibe como parametro
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+            if(this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
+        },
+        colisionTablero: function(){ //Colicion de pelota con bordes del tablero
+                if (this.y < 0 || this.y > this.board.height) {
+                    this.speed_y *= -1;
+                }
+                if (this.x < 0 || this.x > this.board.width) {
+                    this.speed_x *= -1;
+                }
         }
     }
 })();
@@ -67,6 +97,7 @@
 })();
 
 (function(){
+    //Creacion del tablero
     self.BoardView = function(canvas,board){
         this.canvas = canvas;
         this.canvas.width = board.width;
@@ -89,9 +120,41 @@
             if(this.board.playing){
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
+                this.board.ball.colisionTablero();
             }
+        },
+        check_collisions: function(){
+            for (var i = this.board.bars.length - 1; i >= 0; i--){
+                var bar = this.board.bars[i];
+                if(hit(bar, this.board.ball)){
+                    this.board.ball.collision(bar);
+                }
+            };
         }
+    }
+
+    function hit(a,b){
+        //Verifica la colicion entre A y B
+        var hit = false;
+            //Colisiones hirizontales
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            //Colisiones verticales
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
+            //Colisión de a con b
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+                hit = true;
+            }
+            //Colision de b con a
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit = true;
+        }
+        return hit;
     }
 
     function draw(ctx,element){
