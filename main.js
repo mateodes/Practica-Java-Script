990e9ba6fbee020ff60b4bdf17c040514583f218
Mateo Rodriguez <mateoytomas2009@hotmail.com>
2022-02-10 22:22:29 -0300

    Implementacion funcional de movimiento de barras y creacion de la pelota

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function(){
-    self.Board = function(width,height){
+    self.Board = function(width,height){ // Elementos del tablero
         this.width = width;
         this.height = height;
         this.playing = false;
@@ -15,7 +15,18 @@
         return elements;
         }
     }
-
+})();
+(function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        board.ball = this;
+        this.kind = "circle";
+    }
 })();
 
 (function(){
@@ -30,12 +41,13 @@
         this.speed = 10;
     }
 
+// Movimiento de las barras
     self.Bar.prototype = {
         down: function(){
             this.y += this.speed;
         },
         up: function(){
-            this.x -= this.speed;
+            this.y -= this.speed;
         },
         toString: function(){
             return "x: "+ this.x +"y: "+ this.y ;
@@ -53,44 +65,68 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){ //Funcion para borrar las barras antes de volverlas a dibujar en una nueva ubicacion
+            this.ctx.clearRect(0,0,this.board.width,this.board.height);
+        },
         draw: function(){
             for (var i = this.board.elements.length - 1; i >= 0; i--){
                 var el = this.board.elements[i];
                 draw(this.ctx,el);
             }
+        },
+        play: function(){ //Funcion de control del juego sobre la ejecucion
+            this.clean();
+            this.draw();
         }
     }
 
     function draw(ctx,element){
-        if(element !== null && element.hasOwnProperty("kind")){
-            switch(element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x,element.y,element.width,element.height);
-                    break;
-            }
+        switch(element.kind){
+            case "rectangle": //Dibujar barras
+                ctx.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            case "circle": //Dibujar pelota
+                ctx.beginPath();
+                ctx.arc(element.x,element.y,element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
+
 })();
 
-    var board = new Board(800,400);
-    var bar = new Bar(20,100,40,100,board);
-    var bar = new Bar(730,100,40,100,board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board);
+// Creacion de tablero, barras y pelota
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar2 = new Bar(730,100,40,100,board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas,board);
+var ball = new Ball(350,100,10,board);
 
+
+// Evento precionar teclas
 document.addEventListener("keydown",function(ev){
+    ev.preventDefault(); //Evitar mover el navegador
     if(ev.keyCode == 38){
+        // Tecla flecha hacia arriba
+        bar2.up();
+    }else if(ev.keyCode == 40){
+        // Tecla flecha hacia abajo
+        bar2.down();
+    }else if(ev.keyCode === 87){
+        // Tecla W
         bar.up();
-    }
-    else if(ev.keyCode == 40){
+    }else if(ev.keyCode === 83){
+        // Tecla S
         bar.down();
     }
-    console.log(""+bar);
+
 });
 
-window.addEventListener("load",main);
+window.requestAnimationFrame(controller);
 
-function main(){
-
-    board_view.draw();
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
